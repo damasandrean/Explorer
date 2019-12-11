964c2ffa43a6514e76bc03f08334ed54764cafd5
Damas Andrean <andreandamas@gmail.com>
2019-12-12 03:51:19 +0700

    Bottom Navigation

--- app/src/main/java/com/damasandrean/explorer/activities/MainActivity.java
+++ app/src/main/java/com/damasandrean/explorer/activities/MainActivity.java
@@ -1,16 +1,60 @@
 package com.damasandrean.explorer.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.damasandrean.explorer.R;
+import com.damasandrean.explorer.fragments.BerandaFragment;
+import com.damasandrean.explorer.fragments.MenuFragment;
+import com.damasandrean.explorer.fragments.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new BerandaFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_beranda:
+                fragment = new BerandaFragment();
+                break;
+            case R.id.action_menu:
+                fragment = new MenuFragment();
+                break;
+            case R.id.action_user:
+                fragment = new UserFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
