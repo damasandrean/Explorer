83db90963556d7c65109ca6f8d45614b262725c3
Damas Andrean <andreandamas@gmail.com>
2019-12-11 17:45:36 +0700

    Splash Screen

---
+++ app/src/main/java/com/damasandrean/explorer/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.damasandrean.explorer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.damasandrean.explorer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
