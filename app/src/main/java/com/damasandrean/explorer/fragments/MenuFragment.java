0f16cffaff6668d09b5781ee07db594982e4b834
Anasziaulhaq <ziaulhaqanas59@gmail.com>
2019-12-14 11:21:58 +0700

    pembaruan

--- app/src/main/java/com/damasandrean/explorer/fragments/MenuFragment.java
+++ app/src/main/java/com/damasandrean/explorer/fragments/MenuFragment.java
@@ -42,7 +42,9 @@ public class MenuFragment extends Fragment {
             public void onClick(View view) {
                 Intent i = new Intent(getActivity(), DetailActivity.class);
                 i.putExtra("title", "Hotel Singhasari");
-                i.putExtra("desc", "Jl. Ir. Soekarno No.120, Beji, Kec. Batu, Kota Batu, Jawa Timur 65236");
+                i.putExtra("desc", " menyediakan layanan dan fasilitas lain seperti VIP Karaoke"+
+                        "Coffee Shop, Cake Shop, Parkway Health, Swimming Pool " +
+                        "Jl. Ir. Soekarno No.120, Beji, Kec. Batu, Kota Batu, Jawa Timur 65236");
                 startActivity(i);
             }
         });
@@ -51,7 +53,8 @@ public class MenuFragment extends Fragment {
             public void onClick(View view) {
                 Intent i = new Intent(getActivity(), DetailActivity.class);
                 i.putExtra("title", "Hotel Grand City");
-                i.putExtra("desc", "Jl. Bukit Berbunga No.104-108, Sidomulyo, Kec. Batu, Kota Batu, Jawa Timur 65317");
+                i.putExtra("desc", " menyediakan layanan dan fasilitas lain seperti VIP Karaoke,"+
+                        " Bamboo Coffee ShopJl. Bukit Berbunga No.104-108, Sidomulyo, Kec. Batu, Kota Batu, Jawa Timur 65317");
                 startActivity(i);
             }
         });
@@ -83,13 +86,45 @@ public class MenuFragment extends Fragment {
             public void onClick(View view) {
                 Intent i = new Intent(getActivity(), DetailActivity.class);
                 i.putExtra("title", "Wisata Paralayang Batu");
-                i.putExtra("desc", "Kota Batu yang memiliki ketinggian 1.326 mdpl. Tempat ini pada mulanya merupakan tempat yang dijadikan sebagai landasan take-off atlet paralayang.\n" +
-                        "\n" +
-                        "Lokasi ini dijadikan pusat latihan dan event paralayang se-Malang Raya. Diresmikan pada tahun 2000, bersamaan dengan diadakannya event PON VI Jatim " +
-
-                        "Paralayang secara geografis terletak di Desa Songgokerto, Kecamatan Batu, Kota Batu, Jawa Timur.\n" +
-                        "\n" +
-                        "Kawasan ini berbatasan dengan Kecamatan Pujon yang merupakan lokasi bagi salah satu air terjun indah di Batu, Coban Rondo");
+                i.putExtra("desc", "Salah satu wisata yang sedang nge-hits di sini adalah Paralayang Batu Malang, salah satu objek wisata yang menyediakan fasilitas kegiatan yang memicu dan memacu adrenalin." +
+                        " Paralayang Batu Malang bisa membuat anda mengucurkan keringat dingin, bersemangat, berteriak sekaligus berdecak kagum");
+                startActivity(i);
+            }
+        });
+        judul6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), DetailActivity.class);
+                i.putExtra("title", "Wisata Alun Alun Batu ");
+                i.putExtra("desc", "Alun-alun kota Batu Malang Jawa Timur Terbaru adalah satu dari dua alun-alun yang dimiliki Malang. " +
+                        "Alun-alun ini juga disebut dengan Alun-alun Jami’" +
+                        " karena letaknya tepat di depan Masjid Jami’ yang merupakan masjid kebanggaan warga Malang." +
+                        "");
+                startActivity(i);
+            }
+        });
+        judul7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), DetailActivity.class);
+                i.putExtra("title", "Kuliner Warung Wareg Batu ");
+                i.putExtra("desc", "ke kota Batu Malang adalah rumah makan warung wareg. Selain menyediakan masakan tradisional, " +
+                        "rumah makan dengan nama warung wareg ini terkenal dengan berbagai macam masakan olahan" +
+                        " gurami, mulai masakan dari gurami goreng," +
+                        " gurami goreng garing yang dibalut tepung, " +
+                        "hingga gurami bakar dengan berbagai pilihan rasa, " +
+                        "beralamatkan di" +
+                        "Jl. Raya Dieng No.9, Sidomulyo, Kec. Batu, Kota Batu, Jawa Timur 65317");
+                startActivity(i);
+            }
+        });
+        judul8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), DetailActivity.class);
+                i.putExtra("title", "Kuliner Pos Ketan Batu ");
+                i.putExtra("desc", "Saat berwisata ke Kota Malang, salah satu hal yang tak boleh terlewatkan adalah berbagai kuliner khasnya yang menggoda" +
+                        " yakni Pos Ketan Legenda 1967.");
                 startActivity(i);
             }
         });
