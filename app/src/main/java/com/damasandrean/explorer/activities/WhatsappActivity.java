ce997d621252ee0b1698bdc85da2f8705ca97ca2
Damas Andrean <andreandamas@gmail.com>
2019-12-14 16:35:02 +0700

    update button whatsApp

--- app/src/main/java/com/damasandrean/explorer/activities/WhatsappActivity.java
+++ app/src/main/java/com/damasandrean/explorer/activities/WhatsappActivity.java
@@ -8,7 +8,7 @@ import com.damasandrean.explorer.R;
 
 public class WhatsappActivity extends AppCompatActivity {
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
